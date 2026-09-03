c9e2edc0fdd1f1d209f3389649f4ee59e22c154a
adityajagtap976-collab <adityajagtap976@gmail.com>
2026-09-03 19:47:54 +0530

    Initializes basic Python example and gitignore
    Adds `day1.py` to demonstrate fundamental Python data types like strings, integers, floats, and booleans, alongside printing their values and types.
    
    Includes a `.gitignore` file to prevent tracking the Python virtual environment directory.

---
+++ day1.py
@@ -0,0 +1,14 @@
+name = "Aditya"
+age = 19
+gpa = 7.5
+is_smart = True
+
+print(name)
+print(age)
+print(gpa)
+print(is_smart)
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_smart))
