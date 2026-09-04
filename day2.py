c41731c9d52c426a095c67ce9c9e8712503b4028
adityajagtap976-collab <adityajagtap976@gmail.com>
2026-09-04 18:21:51 +0530

    Add Day 2 branching and looping complete

---
+++ day2.py
@@ -0,0 +1,60 @@
+# --- if / elif / else ---
+
+age = 27
+
+if age < 13:
+    catagory = "child"
+elif age < 20:
+    catagory = "teenager"
+else:
+    catagory = "adult"
+
+print(catagory)
+
+# --- for loop ---
+
+numbers = [4, 7, 2, 9, 5]
+
+total = 0
+for number in numbers:
+    total = total + number
+
+print(total)
+
+# --- for loop with range ---
+
+for i in range(5):
+    print(i)
+
+# --- while loop ---
+countdown = 5
+while countdown > 0:
+    print(countdown)
+    countdown = countdown - 1
+
+print("liftoff")
+
+# --- function definition and call ---
+
+
+def classify_age(age):
+    if age < 13:
+        return "child"
+    elif age < 20:
+        return "teenager"
+    else:
+        return "adult"
+
+
+print(classify_age(18))
+print(classify_age(8))
+
+# --- function with default argument ---
+
+
+def greet(name, greeting="Hello"):
+    return greeting + ", " + name
+
+
+print(greet("Sam"))
+print(greet("Sam", "Yo"))
