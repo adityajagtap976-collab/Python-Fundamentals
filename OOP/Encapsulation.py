1a3cf3adc973305f3d86c1dff0631747db40c10d
adityajagtap976-collab <adityajagtap976@gmail.com>
2026-09-10 20:59:33 +0530

    Add OOP Encapsulation

---
+++ OOP/Encapsulation.py
@@ -0,0 +1,26 @@
+class SmartThermostat:
+    def __init__(self, brand, initial_temp):
+        self.brand = brand
+        self.__temperature = initial_temp  # Private attribute
+
+    # Getter method to safely VIEW the temperature
+    def get_temperature(self):
+        return self.__temperature
+
+    # Setter method to safely UPDATE the temperature
+    def set_temperature(self, new_temp):
+        if 60 <= new_temp <= 90:  # Safe range check
+            self.__temperature = new_temp
+            print(f"Temperature set to {self.__temperature}°F")
+        else:
+            print("Temperature out of safe range (60-90°F)")
+
+
+# Creating thermostat object
+my_thermostat = SmartThermostat("Nest", 80)
+
+# Using the getter method to view the temperature
+print(my_thermostat.get_temperature())
+
+# Using the setter method to update the temperature
+my_thermostat.set_temperature(75)
